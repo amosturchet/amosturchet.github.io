27d3abf6d8290cf7d8f8fa88854a960ba75abaf2
amosturchet <turchetamos@gmail.com>
2026-08-01 12:27:09 +0200

    updated npm packages

--- other/node_modules/@types/node/test/reporters.d.ts
+++ other/node_modules/@types/node/test/reporters.d.ts
@@ -1,41 +1,3 @@
-/**
- * The `node:test` module supports passing `--test-reporter`
- * flags for the test runner to use a specific reporter.
- *
- * The following built-reporters are supported:
- *
- * * `spec`
- *   The `spec` reporter outputs the test results in a human-readable format. This
- *   is the default reporter.
- *
- * * `tap`
- *   The `tap` reporter outputs the test results in the [TAP](https://testanything.org/) format.
- *
- * * `dot`
- *   The `dot` reporter outputs the test results in a compact format,
- *   where each passing test is represented by a `.`,
- *   and each failing test is represented by a `X`.
- *
- * * `junit`
- *   The junit reporter outputs test results in a jUnit XML format
- *
- * * `lcov`
- *   The `lcov` reporter outputs test coverage when used with the
- *   `--experimental-test-coverage` flag.
- *
- * The exact output of these reporters is subject to change between versions of
- * Node.js, and should not be relied on programmatically. If programmatic access
- * to the test runner's output is required, use the events emitted by the
- * `TestsStream`.
- *
- * The reporters are available via the `node:test/reporters` module:
- *
- * ```js
- * import { tap, spec, dot, junit, lcov } from 'node:test/reporters';
- * ```
- * @since v19.9.0, v18.17.0
- * @see [source](https://github.com/nodejs/node/blob/v25.x/lib/test/reporters.js)
- */
 declare module "node:test/reporters" {
     import { Transform, TransformOptions } from "node:stream";
     import { EventData } from "node:test";
@@ -46,6 +8,7 @@ declare module "node:test/reporters" {
         | { type: "test:diagnostic"; data: EventData.TestDiagnostic }
         | { type: "test:enqueue"; data: EventData.TestEnqueue }
         | { type: "test:fail"; data: EventData.TestFail }
+        | { type: "test:interrupted"; data: EventData.TestInterrupted }
         | { type: "test:pass"; data: EventData.TestPass }
         | { type: "test:plan"; data: EventData.TestPlan }
         | { type: "test:start"; data: EventData.TestStart }
@@ -88,7 +51,7 @@ declare module "node:test/reporters" {
     }
     /**
      * The `lcov` reporter outputs test coverage when used with the
-     * [`--experimental-test-coverage`](https://nodejs.org/docs/latest-v25.x/api/cli.html#--experimental-test-coverage) flag.
+     * [`--experimental-test-coverage`](https://nodejs.org/docs/latest-v26.x/api/cli.html#--experimental-test-coverage) flag.
      * @since v22.0.0
      */
     const lcov: ReporterConstructorWrapper<typeof LcovReporter>;
